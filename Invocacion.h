5535838c5a7c68757c0cd4e639ed0df54751995a
davidhl4 <dahella4@gmail.com>
2018-03-06 10:03:37 +0100

    Add files via upload

---
+++ Invocacion.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Invocacion.h
+ * Author: dhl4
+ *
+ * Created on 6 de marzo de 2018, 9:56
+ */
+
+#ifndef INVOCACION_H
+#define INVOCACION_H
+
+class Invocacion {
+public:
+    Invocacion();
+    Invocacion(const Invocacion& orig);
+    virtual ~Invocacion();
+private:
+
+};
+
+#endif /* INVOCACION_H */
+
